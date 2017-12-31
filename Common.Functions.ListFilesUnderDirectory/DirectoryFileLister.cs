b8001cccb55f9b5d07d7edf95588f93fcb5e32ee
Christopher Dorst <chris@dorst.email>
2017-12-31 02:47:55 -0600

    Add DirectoryFileLister

---
+++ Common.Functions.ListFilesUnderDirectory/DirectoryFileLister.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using static System.IO.Directory;
+
+namespace Common.Functions.ListFilesUnderDirectory
+{
+    public static class DirectoryFileLister
+    {
+        private const string AllFiles = "*.*";
+
+        public static IEnumerable<string> ListFiles(string path)
+            => EnumerateFiles(path, AllFiles, SearchOption.AllDirectories);
+    }
+}
